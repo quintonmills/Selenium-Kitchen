2c0e3f4466155e282d855550fc24aa44ed33d4c8
Quinton Mills <41127568+quintonmills@users.noreply.github.com>
2018-10-11 13:33:23 -0400

    This is the finshed python test for parameterization
    this test will read test data from a CSV

--- param.py
+++ param.py
@@ -8,6 +8,7 @@ test_status = True
 
 try:
     #create an instance of WebDriver for Firefox
+    driver = webdriver.Firefox(executable_path='/Users/QuintonMills/Desktop')
     driver = webdriver.Firefox()
     driver.get("http://dl.dropbox.com/u/55228056/bmicalculator.html")
     #open csv file
@@ -18,7 +19,7 @@ try:
         test_executed += 1
         print("test "+str(test_executed))
         highlightField = driver.find_element_by_name("heightCMS")
-        heightField.clear()
+        highlightField.clear()
         highlightField.send_keys(row[0])
         weightField = driver.find_element_by_name("weightKg")
         weightField.clear()
@@ -27,6 +28,23 @@ try:
         calculateButton.click()
         bmiLabel = driver.find_element_by_name("bmi")
         bmiCategoryLabel = driver.find_element_by_id("bmi_category")
-        if bmiCategoryLabel.get_attribute("value") == row[2];
-        print("Pass, expected value for BMI <" +)
-        
+        if bmiCategoryLabel.get_attribute("value") == row[2]:
+            print("Pass, expected value for BMI <" + row[2] + ", actual <" + bmiLabel.get_attribute("value") + ">")
+        else:
+            print("fail, expected value for BMI Category <" + row[3] + ", actual <" + bmiLabel.get_attribute("value") + ">")
+            test_status=False
+        if bmiCategoryLabel.get_attribute("value") == row[3]:
+            print("pass, expected value for BMI Category <" + row[3] + ">, actual <" + bmiCategoryLabel.get_attribute("value") + ">")
+        else:
+            print("fail, expected value for BMI Catergory <" + row[3] + ">, actual <" + bmiCategoryLabel.get_attribute("value") + ">")
+            test_status = False
+        if test_status == True:
+            test_passed = test_passed + 1
+        else :
+            test_failed = test_failed +1
+        datafile.close()
+    #except:
+    #    print("Unexpected error:", sys.exc_info()[0])
+    #    raise
+finally:
+        SystemExit()
